056a8946297e7e6b81359bf4f48638fd7b14fc11
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-27 08:53:56 -0300

    error forms added

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
+const errorMessage = document.querySelector(".error")
 
 function validateForm() {
-    const email = document.getElementById("email");
-    console.log(email);
-    card1.classList.toggle("hidden");
-    card2.classList.toggle("hidden");
+    const email = document.getElementById("email").value;
+
+    //regex for email
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (emailPattern.test(email)) {
+        errorMessage.textContent = '';
+        card1.classList.toggle("hidden");
+        card2.classList.toggle("hidden");
+      } else {
+        errorMessage.classList.toggle("hidden");
+      }
+
 }
